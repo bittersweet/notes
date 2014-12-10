4e2095a86006708adcc8d6e44aae8f78c349a2db
Mark Mulder <markmulder@gmail.com>
2014-12-10 14:50:01 +0100

    Go fmt

--- notes.go
+++ notes.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "os"
-  "bufio"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func showSubject(subject string) {
-  path := fmt.Sprintf("/Users/markmulder/dotfiles/notes/%v.txt", subject)
-  file, err := os.Open(path)
-  if err != nil {
-    fmt.Printf("%v\n", err)
-  }
-  defer file.Close()
+	path := fmt.Sprintf("/Users/markmulder/dotfiles/notes/%v.txt", subject)
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    fmt.Println(scanner.Text())
-  }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
 
 func main() {
-  flag.Parse()
-  subject := flag.Arg(0)
-  if len(subject) == 0 {
-    fmt.Printf("No subject given, exiting\n")
-  } else {
-    showSubject(subject)
-  }
+	flag.Parse()
+	subject := flag.Arg(0)
+	if len(subject) == 0 {
+		fmt.Printf("No subject given, exiting\n")
+	} else {
+		showSubject(subject)
+	}
 }
